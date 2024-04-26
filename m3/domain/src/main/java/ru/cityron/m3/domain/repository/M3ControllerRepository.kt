9753a5554a5ab4bf38e615a18fb224bd6ed983e7
Павел <59141267+VopRos-ZP@users.noreply.github.com>
2024-04-26 22:38:00 +0300

    Delete m3 directory

--- m3/domain/src/main/java/ru/cityron/m3/domain/repository/M3ControllerRepository.kt
+++
@@ -1,6 +0,0 @@
-package ru.cityron.m3.domain.repository
-
-import ru.cityron.core.domain.repository.IControllerRepository
-import ru.cityron.m3.domain.model.M3All
-
-interface M3ControllerRepository : IControllerRepository<M3All>
