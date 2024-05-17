853086ecb272bbb55bb2ec5789b17947bdba989f
VopRos-ZP <zolpavel366@gmail.com>
2024-05-17 13:02:08 +0300

    Changes theme

--- app/src/main/java/ru/cityron/ui/theme/Color.kt
+++ app/src/main/java/ru/cityron/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package ru.cityron.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Orange = Color(0xFFFFA826)
+val Blue = Color(0xFFBADBFF)
+val Black = Color(0xFF1D1D1D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green = Color(0xFF318823)
+val Red = Color(0xFFAB2929)
--- app/src/main/java/ru/cityron/ui/theme/Theme.kt
+++ app/src/main/java/ru/cityron/ui/theme/Theme.kt
@@ -1,9 +1,5 @@
 package ru.cityron.ui.theme
 
-//import androidx.compose.material.darkColorScheme
-//import androidx.compose.material.dynamicDarkColorScheme
-//import androidx.compose.material.dynamicLightColorScheme
-//import androidx.compose.material.lightColorScheme
 import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
@@ -15,43 +11,12 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-//private val DarkColorScheme = darkColorScheme(
-//    primary = Purple80,
-//    secondary = PurpleGrey80,
-//    tertiary = Pink80
-//)
-//
-//private val LightColorScheme = lightColorScheme(
-//    primary = Purple40,
-//    secondary = PurpleGrey40,
-//    tertiary = Pink40
-//
-//    /* Other default colors to override
-//    background = Color(0xFFFFFBFE),
-//    surface = Color(0xFFFFFBFE),
-//    onPrimary = Color.White,
-//    onSecondary = Color.White,
-//    onTertiary = Color.White,
-//    onBackground = Color(0xFF1C1B1F),
-//    onSurface = Color(0xFF1C1B1F),
-//    */
-//)
-
 @Composable
 fun CityronTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//            val context = LocalContext.current
-//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = DarkColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
@@ -60,7 +25,6 @@ fun CityronTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
-
     MaterialTheme(
         colors = colorScheme,
         typography = Typography,
@@ -69,11 +33,12 @@ fun CityronTheme(
 }
 
 private val DarkColorScheme = darkColors(
-    primary = Purple80,
-    secondary = PurpleGrey80,
+    primary = Black,
+    secondary = Orange,
 )
 
-private val LightColorScheme = lightColors(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-)
+// Если будет светлая тема
+//private val LightColorScheme = lightColors(
+//    primary = Purple40,
+//    secondary = PurpleGrey40,
+//)
